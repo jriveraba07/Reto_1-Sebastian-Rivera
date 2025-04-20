a6023ceb14542f097f449f2f32cfde743fd37596
Sebastian Rivera <jriveraba@unal.edu.co>
2025-04-20 16:00:34 -0500

    Ejercicios solicitados
    Los 5 Ejercicios ya resueltos

---
+++ Ejercicio - 1.py
@@ -0,0 +1,31 @@
+# Ejercicio - 1
+
+#* Una funcion que realice operaciones basicas:
+#* (suma, resta, multiplicacion, division) entre dos numeros segun la eleccion del usuario, 
+#* La forma de entrada de la función será los dos operandos y el caracter usado para la operación.
+#* Ej: entrada: (1,2,"+"), salida (3).
+
+#* Función 
+
+def operacion_basica(a, b, operacion):
+    if operacion == "+": 
+        return a + b
+    elif operacion == "-":
+        return a - b
+    elif operacion == "*":
+        return a * b
+    elif operacion == "/":
+        return a / b
+    else:
+        return "caracter no valido"
+
+#* programa principal
+
+x = float(input("introduce el primer numero: "))
+y = float(input("introduce el segundo numero: "))
+operador = str(input("introduce la operación a realizar (+, -, *, /): "))
+resultado = operacion_basica(x, y, operador)
+
+#* salida
+
+print("el resultado es:", resultado)
--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Reto_1-Sebastian Rivera 
  Reto 1 de POO grupo (4) con Felipe Gonzalez Roldan
+ 
